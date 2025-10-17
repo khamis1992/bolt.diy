099fe3cafc97a3cb95fcbde2c604611b42ad857a
khamis1992 <khamis-1992@hotmail.com>
2025-10-17 17:05:52 +0300

    docs: add development requirements guide

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,6 +2,7 @@ import type { ServerBuild } from '@remix-run/cloudflare';
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
 export const onRequest: PagesFunction = async (context) => {
+  // @ts-expect-error The build artifact is generated at runtime by the Remix build step
   const serverBuild = (await import('../build/server')) as unknown as ServerBuild;
 
   const handler = createPagesFunctionHandler({
